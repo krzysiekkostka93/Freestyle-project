c78671ecc146a132edf76091353458f9dd2583ea
Krzysztof Bender <krzysztof.bender@gmail.com>
2022-02-15 10:06:03 +0100

    Sample java

---
+++ bash/basic/functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pole_prostokata() {
+	area=$(($1 * $2))
+	echo "Pole to: $area"
+}
+
+pole_prostokata 10 20
---
+++ projekt_nginx/Readme.md
@@ -0,0 +1,2 @@
+java -jar helloworld-0.0.2-SNAPSHOT.jar --server.port=8083
+
