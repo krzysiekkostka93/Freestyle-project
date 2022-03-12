c08a8e6560ef63e4f9ce6c6cee02a084104613f7
Łukasz Pieczonka <lukas@lu-media.pl>
2022-03-12 11:12:10 +0100

    up docker-compose

--- docker/LAB/lms/docker-entrypoint.sh
+++ docker/LAB/lms/docker-entrypoint.sh
@@ -12,7 +12,7 @@ else
     sed -i -e 's/; type               = mysql/type               = ${SQL_TYPE}/' ${LMS_INI_SAMPLE}
     sed -i -e 's/; host               = localhost/host               = ${MYSQL_HOST}/' ${LMS_INI_SAMPLE}
     sed -i -e 's/; user               = mysql/user               = ${MYSQL_USER}/' ${LMS_INI_SAMPLE}
-    sed -i -e 's/; password          = password/password          = ${MYSQL_PASS}/' ${LMS_INI_SAMPLE}
+    sed -i -e 's/; password          = password/password          = ${MYSQL_PASSWORD}/' ${LMS_INI_SAMPLE}
     sed -i -e 's/; database          = lms/database          = ${MYSQL_DATABASE}/' ${LMS_INI_SAMPLE}
     mkdir -p /etc/lms
     envsubst < ${LMS_INI_SAMPLE} >> ${LMS_INI_FILE}
